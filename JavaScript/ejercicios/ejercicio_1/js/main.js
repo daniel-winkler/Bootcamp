d24ee31e26f25c3b244364a59e878393490c47ff
dwinkler <danielbwo@gmail.com>
2021-04-07 12:01:42 +0200

    terminamos de corregir el ej 1 del DOM

--- JavaScript/ejercicios/ejercicio_1/js/main.js
+++ JavaScript/ejercicios/ejercicio_1/js/main.js
@@ -1,4 +1,4 @@
-/*Apartado 1 - Partimos de un HTML con un div vacío y con JS añadimos
+/*APARTADO 1 - Partimos de un HTML con un div vacío y con JS añadimos
 dos elementos p con texto dentro.*/
 
 const voidDiv = document.getElementsByTagName("div")[0];
@@ -14,7 +14,7 @@ const newP2 = newP.cloneNode(true);
 
 voidDiv.append(newP, newP2);
 
-/*Apartado 2 - Al pulsar un botón, cambiar el color del fondo del cuerpo de HTML.*/
+/*APARTADO 2 - Al pulsar un botón, cambiar el color del fondo del cuerpo de HTML.*/
 
 const colorButton = document.getElementsByTagName("button")[0];
 
@@ -22,7 +22,7 @@ colorButton.addEventListener("click", function () {
     document.body.classList.toggle("bg-green");
 });
 
-/*Apartado 3 - Partimos de un HTML con una lista de 3 URLs(texto) de imágenes y un elementimg.
+/*APARTADO 3 - Partimos de un HTML con una lista de 3 URLs(texto) de imágenes y un elementimg.
 Al hacer clicken cada URL, cambiará la imagen a la que contenga dicha URL*/
 
 const img = document.getElementsByTagName("img")[0];
@@ -43,7 +43,7 @@ list.addEventListener("click", function (e) {
     img.setAttribute("src", e.target.textContent)
 });
 
-/*Apartado 4 - Añadir un input de tipo texto y un botón. Al pulsar el botón debe 
+/*APARTADO 4 - Añadir un input de tipo texto y un botón. Al pulsar el botón debe 
 escribirse el texto del input en un párrafo.*/
 
 const changeText = document.getElementsByTagName("button")[1];
@@ -62,7 +62,7 @@ changeText.addEventListener("click", () => {
 });
 
 
-/*Apartado 4.2 - Añadir un nuevo input pero esta vez cambiará el texto con cada 
+/*APARTADO 4.2 - Añadir un nuevo input pero esta vez cambiará el texto con cada 
 pulsación de tecla del usuario.*/
 
 const inputText2 = document.getElementsByTagName("input")[1];
@@ -71,7 +71,7 @@ const keyInput = document.getElementsByTagName("h3")[1]
 inputText2.addEventListener("input", () => keyInput.textContent = inputText2.value);
                             //keyup o keydown tambien son opciones, pero son menos flexibles
 
-/*Apartado 5 - De forma similar al anterior, pero para un textarea y validará si lo introducido es mayor de 15 caracteres.*/
+/*APARTADO 5 - De forma similar al anterior, pero para un textarea y validará si lo introducido es mayor de 15 caracteres.*/
 
 const textArea = document.querySelector("#textarea");
 const validarTexto = document.getElementsByTagName("button")[2];
@@ -94,7 +94,7 @@ textArea.addEventListener("input", e => {
     e.target.style.color = e.target.value.length > 15 ? "red" : "green";
 });
 
-/*Apartado 6 - Añadir un input de tipo texto con leyenda: “ Escribir un número par”.
+/*APARTADO 6 - Añadir un input de tipo texto con leyenda: “ Escribir un número par”.
 Añadir un botón. Al pulsar el botón nos validará si el número es par o no. 
 En caso negativo, cambiar los bordes del input a rojo.*/
 
@@ -115,7 +115,7 @@ buttonPar.addEventListener("click", function () { //aqui no usamos e.target porq
 
 inputPar.addEventListener("keydown", () => inputPar.style.color = "revert");
 
-/*Apartado 7 - Partiendo de una lista ul , crear 10 li con un texto indicando el número del elemento (“Elemento X”)
+/*APARTADO 7 - Partiendo de una lista ul , crear 10 li con un texto indicando el número del elemento (“Elemento X”)
 usando un bucle for*/
 
 const list2 = document.getElementsByTagName("ul")[1];
@@ -127,7 +127,7 @@ for (i = 0 ; i <= 9 ; i++) {
     list2.children[i].textContent = `Elemento ${i + 1}`;
 };
 
-/*Apartado 8 - Crear un enlace y un botón. Si pulso el enlace se me abre la URL en la misma página. Si pulso primero el
+/*APARTADO 8 - Crear un enlace y un botón. Si pulso el enlace se me abre la URL en la misma página. Si pulso primero el
 botón y luego el enlace, se me abre en una nueva pestaña.*/
 
 const linkGoogle = document.querySelector("#linkgoogle");
@@ -144,7 +144,7 @@ const buttonGoogle = document.querySelector("#buttongoogle");
 
 buttonGoogle.addEventListener("click", () => linkGoogle.target = "_blank");
 
-/*Apartado 9 - Añadir un párrafo y un select con 5 opciones de colores: negro, blanco, rojo, amarillo, verde y azul. Al
+/*APARTADO 9 - Añadir un párrafo y un select con 5 opciones de colores: negro, blanco, rojo, amarillo, verde y azul. Al
 seleccionar un color del select , cambiar el color del parrafo.*/
 
 const parrafo = document.getElementsByTagName("p")[2];
@@ -153,13 +153,28 @@ const color = document.getElementsByTagName("select")[0];
 
 color.addEventListener("change", function (e) { // "input" tambien es una opcion
     parrafo.style.color = e.target.value;
-    parrafo.textContent = `Este texto es de color ${e.target.textContent}`; //???
+    parrafo.textContent = `Este texto es de color ${e.target.value}`; //???
+    console.log(e.target);
 });
 
 
-//Apartado 10 - Incluir un botón que al pulsarlo genere un número aleatorio y mantenga en una lista actualiza el número
-//de elementos que se han generado, los que son pares y los que son impares.
+/*APARTADO 10 - Incluir un botón que al pulsarlo genere un número aleatorio y mantenga en una lista actualiza el número
+de elementos que se han generado, los que son pares y los que son impares.*/
 
+const num = document.getElementById("currentRandom")
+const totalNumbers = document.getElementById("totalNumbers")
+const oddNumbers = document.getElementById("oddNumbers")
+const evenNumbers = document.getElementById("evenNumbers")
+
+document.getElementById("randomButton").onclick = () => {
+    const randomNumber = Math.floor(Math.random() * 100);
+    currentRandom.textContent = randomNumber;
+    // totalNumbers.textContent = Number(totalNumbers.textContent) + 1;
+    totalNumbers.textContent++; //por algun motivo JS reconoce que es un numero y funciona tal cual con ++
+    randomNumber % 2 === 0 ? evenNumbers.textContent++ : oddNumbers.textContent++;
+}
+
+//OTRA SOLUCION (mi primera)
 const rngButton = document.getElementById("rng");
 const rngInfo = document.querySelector("#rnginfo");
 const listEven = document.querySelector("#listeven");
@@ -184,9 +199,9 @@ rngButton.addEventListener("click", function () {
     rngInfo.textContent = `Se han generado ${evenArray.length + oddArray.length} números, de los cuales ${evenArray.length} son pares y ${oddArray.length} son impares`;
 });
 
-//Apartado 11 - Construir una lista que tenga números. Añadir un input donde poder añadir números y un botón. Al pulsar
-//el botón, si el número ya existe en la lista, mostrar un mensaje de error, si no existe, lo añadirá al
-//principio.
+/*APARTADO 11 - Construir una lista que tenga números. Añadir un input donde poder añadir números y un botón. Al pulsar
+el botón, si el número ya existe en la lista, mostrar un mensaje de error, si no existe, lo añadirá al
+principio. AUN PENDIENTE DE HACER*/
 
 let numList = document.querySelector("#numlist");
 const numButton = document.querySelector("#numbutton");
@@ -202,3 +217,40 @@ numButton.addEventListener("click", function() {
     }
     console.log(numArray);
 });
+[].indexOf
+
+/*APARTADO 12 - Crearemos una clase .btnen CSS que le de ciertos estilos a un botón. Al hacer clicken el botón haremos 
+“toggle” o alternaremos esa clase, es decir, si está presente la quitaremos y si no está, la añadiremos*/
+
+document.getElementById("toggler").onclick = (e) => e.target.classList.toggle("btn");
+
+/*EXTRA - REFACTORIZAR CODIGO*/
+
+let buttons = document.getElementsByClassName("btn-red"); //Devuelve HTML Collection
+
+// buttons[0].addEventListener('click', () => {
+//     buttons[0].style.backgroundColor = "red";
+// });
+
+// buttons[1].addEventListener('click', () => {
+//     buttons[1].style.backgroundColor = "red";
+// });
+
+// buttons[2].addEventListener('click', () => {
+//     buttons[2].style.backgroundColor = "red";
+// });
+
+//Solucion 1
+
+Array.from(buttons).forEach(button => button.onclick = e => e.target.style.backgroundColor = "red")
+
+//Solucion 2
+
+buttons = document.querySelectorAll(".btn-red"); //devuelve NodeList que tiene disponible para forEach
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", function (e) {
+        e.target.style.backgroundColor = "red";
+    })
+})
