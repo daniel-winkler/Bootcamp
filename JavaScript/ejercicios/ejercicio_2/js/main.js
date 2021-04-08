b3f07bac1f360baedf277b87487a514a92529697
dwinkler <danielbwo@gmail.com>
2021-04-08 11:38:28 +0200

    terminamos de resolver el ej 2

--- JavaScript/ejercicios/ejercicio_2/js/main.js
+++ JavaScript/ejercicios/ejercicio_2/js/main.js
@@ -1,13 +1,13 @@
 /* CONSTRUCTOR */
 
-function User(name, firstLastName, secondLastName, email, age, city, productsCount) {
+function User(name, firstLastName, secondLastName, email, age, city) {
     this.name = name;
     this.firstLastName = firstLastName;
     this.secondLastName = secondLastName;
     this.email = email;
     this.age = age;
     this.city = city;
-    this.productsCount = productsCount;
+    this.productsCount = 0;
     this.incrementProducts =  () => this.productsCount++;
     this.emptyProducts = () => this.productsCount = 0;
 };
@@ -32,9 +32,65 @@ users.forEach(user => {
     let option = document.createElement("option");
     option.textContent = user.name;
     select.appendChild(option)
- })
-console.log(select);
+});
 
+// for (let user of users) {
+//     let option = document.createElement("option");
+//     option.textContent = user.name;
+//     select.appendChild(option)
+// } ALTERNATIVA FOR OF
+
+for (const key in users[0]) {
+    const value = users[0][key];
+    if (typeof value !== "function") {
+        const newListItem = document.createElement("li");
+        // newListItem.textContent = `${key}: ${value}`;
+        newListItem.innerHTML = `<b>${key}:</b> ${value}`;
+        newListItem.classList.add("list-group-item")
+        list.appendChild(newListItem)
+        // list.innerHTML = `<li class="list-group-item"><b>${key}:</b> ${value}</li>`;
+    };
+};
+
+
+/*RELLENAR LA LISTA CON LA INFO DE UN USUARIO*/
+
+function fillList(user) {
+
+    list.innerHTML = "";
+
+    for (const key in user) {
+
+        const value = user[key]; //usamos [] porque va iterando en las propiedades de cada user.
+
+        if (typeof value !== "function") {
+
+            list.innerHTML += `<li class="list-group-item"><b>${key}:</b> ${value}</li>`;
+        };
+    };
+};
+
+
+/*AÑADIR LISTENERS*/
+
+select.addEventListener("change", () => {
+    const selectedUser = users.find(user => user.name === select.value);
+    //va buscando en nuestro array el nombre del nuestro objeto hasta que coincida con el nombre seleccionado en nuestro select
+   fillList(selectedUser)
+});
+
+incrementButton.addEventListener("click", processProducts);
+emptyButton.addEventListener("click", processProducts);
+
+function processProducts(e) {
+    const selectedUser = users.find(user => user.name === select.value);
+    if (e.target.id === "incrementbutton") {
+        selectedUser.incrementProducts();
+    } else {
+        selectedUser.emptyProducts();
+    };
+    fillList(selectedUser)
+}
 
 // const user1 = {
 //     name: "Fran",
