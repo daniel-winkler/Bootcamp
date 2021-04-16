9b5e75a3018afe860b330da0323f0f13d6b32cba
dwinkler <danielbwo@gmail.com>
2021-04-16 13:49:53 +0200

    retoques del ej 6 de js y resolvemos el apartado 2

--- JavaScript/ejercicios/ejercicio_6/js/main.js
+++ JavaScript/ejercicios/ejercicio_6/js/main.js
@@ -34,6 +34,9 @@ const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 
 const postsArray = [];
 
+let pageSize = 20;
+let pageNumber = 1;
+    
 function pushArray(array) {
     array.forEach(post => postsArray.push(post))
 }
@@ -43,16 +46,17 @@ function fillData(currentPageArray) {
     currentPageArray.forEach(post => {
         tbody.innerHTML +=
         `<tr>
-        <td>
-        <p><b>${post.id} - ${post.title}</b></p>
-        <p>${post.body}</p>
-        </td>
+            <td>
+                <p><b>${post.id} - ${post.title}</b></p>
+                <p>${post.body}</p>
+            </td>
         </tr>`
     })
-    pageCounter.textContent = `Page ${pageNumber}/5`
+    pageCounter.textContent = `Page ${pageNumber} / ${postsArray.length / pageSize}`
 }
 
 function paginator(items, current_page, per_page_items) {
+    //Source: https://shouts.dev/easiest-way-to-paginate-an-array-in-javascript
     let page = current_page || 1,
 	per_page = per_page_items || 10,
 	offset = (page - 1) * per_page,
@@ -71,35 +75,32 @@ function paginator(items, current_page, per_page_items) {
 	};
 }
 
-let pageNumber = 1;
+// function paginate(array, page_size, page_number) {
+//     //Source: https://stackoverflow.com/questions/42761068/paginate-javascript-array
+//     // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
+//     return array.slice((page_number - 1) * page_size, page_number * page_size);
+//   }
+  
+//   console.log(paginate([1, 2, 3, 4, 5, 6], 2, 2));
+//   console.log(paginate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4, 1));
 
 function previousPage() {
     if (pageNumber > 1) {
         pageNumber--
-        fillData(paginator(postsArray,pageNumber,20).data)
+        fillData(paginator(postsArray,pageNumber,pageSize).data)
     } else {
         alert("No hay más páginas")
     }
 }
 
 function nextPage() {
-    if (pageNumber < 5) {
+    if (pageNumber < postsArray.length / pageSize) {
         pageNumber++
-        fillData(paginator(postsArray,pageNumber,20).data)
+        fillData(paginator(postsArray,pageNumber,pageSize).data)
     } else {
         alert("No hay más páginas")
     }
 }
-
-// async function requestURL(url) {
-    //     const response = await fetch(url);
-    //     const data = await response.json();
-    //     fillData(data);
-//     pushArray(data);
-//     console.log(postsArray);
-// }
-// requestURL(POSTS_URL)
-
 
 fetch(POSTS_URL)
     .then(response => {
@@ -112,7 +113,85 @@ fetch(POSTS_URL)
         pushArray(data)
         previousButton.addEventListener("click", previousPage)
         nextButton.addEventListener("click", nextPage)
-        fillData(paginator(postsArray,pageNumber,20).data)
+        fillData(paginator(postsArray,pageNumber,pageSize).data)
     })
     .catch(error => console.log(error))
 ;
+
+
+/* APARTADO 2 */
+
+const LOGIN_URL = "https://reqres.in/api/login";
+
+const USERS_URL = "https://reqres.in/api/users"
+
+const emailInput = document.querySelectorAll("input")[0];
+const passwordInput = document.querySelectorAll("input")[1];
+const usersList = document.querySelector("#usersList");
+
+document.querySelector("#loginButton").addEventListener("click" , login);
+
+function login(e) {
+    e.preventDefault()
+    const userInfo = {
+        email: emailInput.value,
+        password: passwordInput.value
+    };
+    // console.log(userInfo);
+    const config = {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(userInfo)
+    }
+
+    fetch(LOGIN_URL, config)
+    .then(r => r.json())//console.log(r)
+    // .then(data => {
+    //     if (data.error) {
+    //         alert(data.error)
+    //     } else {
+    //         fetchAllUsers()
+    //     }
+    // })
+    .then(data => data.error ? alert(data.error) : fetchAllUsers())
+}
+
+async function fetchAllUsers() {
+    // fetch(USERS_URL)
+    // .then(r => r.json())
+    // .then(data => console.log(data))
+
+    let response = await fetch(USERS_URL);
+    let data = await response.json();
+    // console.log(data);
+    let users = [];
+
+    for (let page = 1 ; page <= data.total_pages ; page++) {
+        let response = await fetch(`${USERS_URL}?page=${page}`);
+        let newData = await response.json();
+        users = users.concat(newData.data) // users = [...users, ...newData.data]
+    }
+    console.log(users);
+
+    usersList.innerHTML = "";
+    users.forEach(user => usersList.innerHTML += `<li>${user.email}</li>`)
+}
+
+// Alternativa oara recuperar todas las paginas de una API utlizando .then()
+
+let users = [];
+
+function fetchAllUsersV2(url) {
+    fetch(url)
+    .then(r => r.json())
+    .then(newData => {
+        users = users.concat(newData.data);
+
+        if (newData.page < newData.total_pages) {
+            fetchAllUsersV2(`${USERS_URL}?page=${newData.page+1}`)
+        } else {
+            usersList.innerHTML = "";
+            users.forEach(user => usersList.innerHTML += `<li>${user.email}</li>`)
+        }
+    });
+}
