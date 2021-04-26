04c8f7fd3028573f5df559980e86fb389b3302ce
dwinkler <danielbwo@gmail.com>
2021-04-26 14:01:14 +0200

    introducimos useContext

--- React/cheatsheet/my-app/src/App.js
+++ React/cheatsheet/my-app/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import Counter from './components/Counter';
 import FirstComponent, {SecondComponent, ThirdComponent} from './components/FirstComponent';
 import UseEffectComponent from './components/UseEffectComponent';
-import { useState } from "react";
+import UseContextComponent from './components/UseContextComponent';
+import { useState, createContext } from "react";
+
+export const GlobalContext = createContext()
+
 
 function App() {
 
@@ -10,17 +14,25 @@ function App() {
 
   return (
     <div className="App">
-      <h1>useEffect Component</h1>
+
+      <GlobalContext.Provider value="Soy un string guardado en un contexto">
+        <UseContextComponent />
+      </GlobalContext.Provider>
+
+      {/* <h1>useEffect Component</h1>
       {show && <UseEffectComponent />}
       <button onClick={() => setShow(!show)}>Show</button>
-      {/* <h1>Counter</h1>
+
+      <h1>Counter</h1>
       <Counter />
+
       <h1>First Component</h1>
       <FirstComponent date="16 de Agosto de 2025"></FirstComponent>
       <FirstComponent title="título enviado desde el padre" date="16 de Agosto de 2025"/>
       <FirstComponent title="otro titulo" otherAttr={21} />
       <SecondComponent array={[23, 65]}/>
       <ThirdComponent /> */}
+
     </div>
   );
 }
