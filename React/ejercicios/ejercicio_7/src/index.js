4976cdfc0467c4c4c95af7fa435cbec09bdd0b22
dwinkler <danielbwo@gmail.com>
2021-04-28 16:53:59 +0200

    iniciamos ej 7 de react

---
+++ React/ejercicios/ejercicio_7/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+//TMDb API Key: 7d34055c400374a3555f3a9f651b9d0d
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
